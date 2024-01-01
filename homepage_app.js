f8e3a6fd2d2c372038c0627e49d20f1e24d8fef1
Sahand <mohsen.301933@gmail.com>
2024-01-01 14:28:31 +0330

    Best_of_btn fixed
    CSS and JS were updated

--- homepage_app.js
+++ homepage_app.js
@@ -1,15 +1,19 @@
 function serial_btn() {
-    document.getElementById("serial_btn").style.backgroundColor = "rgb(21, 21, 21)";
-    document.getElementById("serial_btn").style.fontWeight = "normal";
-    document.getElementById("movie_btn").style.backgroundColor = "rgb(58, 58, 58)";
-    document.getElementById("movie_btn").style.fontWeight = "bold";
+    if (document.getElementById("movie_btn").style.backgroundColor !== "rgb(21, 21, 21)") {
+        document.getElementById("serial_btn").style.fontWeight = "bold";
+        document.getElementById("serial_btn").style.backgroundColor = "rgb(58, 58, 58)";
+        document.getElementById("movie_btn").style.fontWeight = "normal";
+        document.getElementById("movie_btn").style.backgroundColor = "rgb(21, 21, 21)";
+    }
 }
 
 function movie_btn() {
-    document.getElementById("movie_btn").style.backgroundColor = "rgb(21, 21, 21)";
-    document.getElementById("movie_btn").style.fontWeight = "normal";
-    document.getElementById("serial_btn").style.backgroundColor = "rgb(58, 58, 58)";
-    document.getElementById("serial_btn").style.fontWeight = "bold";
+    if (document.getElementById("serial_btn").style.backgroundColor !== "rgb(21, 21, 21)") {
+        document.getElementById("movie_btn").style.fontWeight = "bold";
+        document.getElementById("movie_btn").style.backgroundColor = "rgb(58, 58, 58)";
+        document.getElementById("serial_btn").style.fontWeight = "normal";
+        document.getElementById("serial_btn").style.backgroundColor = "rgb(21, 21, 21)";
+    }
 }
 
 const navbar = document.getElementById("navbar");
